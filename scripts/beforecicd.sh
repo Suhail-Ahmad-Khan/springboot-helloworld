e5587e9ca1433a56b4e97630bd9f21a6b32851e0
khansuhl <khansuhl@gmail.com>
2018-05-08 17:09:55 +0530

    testing deployment application revision

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -7,7 +7,7 @@ sudo su -c "cd /opt/tomcat9/bin;
 	    ./startup.sh"
 
 sudo su -c "aws s3 ls s3://jenkins-war-upload --region ap-south-1"
-sudo su -c "aws s3 cp s3://jenkins-war-upload/#18-8891273898490606592.zip /home/ubuntu/fundooPay.zip --region ap-south-1"
+sudo su -c "aws s3 cp s3://jenkins-war-upload/#44-1111953917755282533.zip /home/ubuntu/fundooPay.zip --region ap-south-1"
 
 sudo su -c "mkdir /home/ubuntu/fundooPay"
 sudo su -c "unzip -o /home/ubuntu/fundooPay.zip -d /home/ubuntu/fundooPay"
