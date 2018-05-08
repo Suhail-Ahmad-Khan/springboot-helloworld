06d6b8a3acd5c2ff5e392be5ed3c2deeda3698cf
khansuhl <khansuhl@gmail.com>
2018-05-08 12:26:09 +0530

    modified beforecicd shell file

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-pwd
-sudo su -c "whoami"
 sudo su -c "apt-get install unzip"
 sudo su -c "apt-get update"
 sudo su -c "service codedeploy-agent start"
+sudo su -c "cd /opt/tomcat9/bin;
+	    ./startup.sh"
+
+aws s3 cp s3://jenkins-war-upload/#18-8891273898490606592.zip fundooPay.zip
+
 
