91757ddd421e5dc820dabb644cc81216b6fd1e53
khansuhl <khansuhl@gmail.com>
2018-05-08 15:40:20 +0530

    modified beforecicd shell file

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -9,9 +9,9 @@ sudo su -c "cd /opt/tomcat9/bin;
 sudo su -c "aws s3 ls s3://jenkins-war-upload --region ap-south-1"
 sudo su -c "aws s3 cp s3://jenkins-war-upload/#18-8891273898490606592.zip /home/ubuntu/fundooPay.zip --region ap-south-1"
 
-sudo su -c "mkdir fundooPay"
-sudo su -c "unzip -o fundooPay.zip -d fundooPay"
-sudo su -c "cd fundooPay/target;
+sudo su -c "mkdir /home/ubuntu/fundooPay"
+sudo su -c "unzip -o /home/ubuntu/fundooPay.zip -d /home/ubuntu/fundooPay"
+sudo su -c "cd /home/ubuntu/fundooPay/target;
 	    mv springboot-helloworld-0.0.1-SNAPSHOT hello.war;
 	    cp hello.war /opt/tomcat9/webapps/"
 
