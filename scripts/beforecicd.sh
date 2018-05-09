041e3fe968523791e145bc4d5842327b116978f8
khansuhl <khansuhl@gmail.com>
2018-05-09 10:27:10 +0530

    trying to go complete auto

--- scripts/beforecicd.sh
+++ scripts/beforecicd.sh
@@ -6,8 +6,9 @@ sudo su -c "service codedeploy-agent start"
 sudo su -c "cd /opt/tomcat9/bin;
 	    ./startup.sh"
 
-sudo su -c "aws s3 ls s3://jenkins-war-upload --region ap-south-1"
-sudo su -c "aws s3 cp s3://jenkins-war-upload/#44-1111953917755282533.zip /home/ubuntu/fundooPay.zip --region ap-south-1"
+#sudo su -c "aws s3 ls s3://jenkins-war-upload --region ap-south-1"
+KEY="$(aws s3 ls s3://jenkins-war-upload --recursive --region ap-south-1 | sort | tail -n 1 | awk '{print $4}')"
+sudo su -c "aws s3 cp s3://jenkins-war-upload/$KEY /home/ubuntu/fundooPay.zip --region ap-south-1"
 
 sudo su -c "mkdir /home/ubuntu/fundooPay"
 sudo su -c "unzip -o /home/ubuntu/fundooPay.zip -d /home/ubuntu/fundooPay"
